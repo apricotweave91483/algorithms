e30d55d9c46fc87de173e873bca36882577bac35
apricotweave91483 <243370722+apricotweave91483@users.noreply.github.com>
2025-12-30 23:20:27 -0800

    digits can be unsigned chars

--- C_leetcodes/palindrome_number.c
+++ C_leetcodes/palindrome_number.c
@@ -2,29 +2,35 @@
 #include <stdlib.h>
 int main() {
 	int x; scanf("%d", &x);
+
 	if (x < 0) {
 		printf("NO\n");
 		return 0;
 	}
+
 	int len = 0;
 	int t = x;
 	while (t) {
 		len++;
 		t /= 10;
 	}
-	short* arr = malloc(sizeof(short) * len);
+
+	unsigned char* arr = malloc(sizeof(unsigned char) * len);
+
 	int i = len - 1;
 	while (x) {
 		arr[i] = x % 10;
 		x /= 10;
 		i--;
 	}
+
 	for (int x = 0; x < (len / 2); ++x) {
 		if (arr[x] != arr[len - x - 1]) {
 			printf("NO\n");
 			return 0;
 		}
 	}
+
 	printf("YES\n");
 	return 0;
 }
